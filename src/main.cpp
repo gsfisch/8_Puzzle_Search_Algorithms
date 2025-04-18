deb4bfea37a830be358c255fba5910f1c157a88e
orgb <gsfisch@inf.ufrgs.br>
2025-04-17 21:59:35 -0300

    15 Puzzle driver for instance file

--- src/main.cpp
+++ src/main.cpp
@@ -225,73 +225,40 @@ int main(int argc, char *argv[])
 
 */
 
-// Driver for  8 Puzzle with instances in file
+// Driver for 15 Puzzle with instances in file
 // Example call:
 // ./main -bfs
 int main(int argc, char *argv[])
 {
-    std::ifstream input("instances/8puzzle_instances.txt");
-    unsigned short state[9];
-    unsigned short parent_state[9] = {
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0};
+    std::ifstream input("instances/15puzzle_instances.txt");
+    unsigned short state_15[16];
+    unsigned short parent_state_15[16] = {
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+    0, 0, 0, 0};
     SEARCH_INFO return_info;
     std::chrono::microseconds duration;
     std::chrono::seconds duration_sec;
 
     // Read each line's first element
-    while(input >> state[0])
+    while(input >> state_15[0])
     {
+        //std::cout << state_15[0] << " ";
         // Fill the last elements
-        for(int i = 1; i < 9; i++)
-            input >> state[i];
-
-        // BFS
-        if(!strcmp(argv[1], "-bfs"))
+        for(int i = 1; i < 16; i++)
         {
-            auto start = std::chrono::high_resolution_clock::now();
-            return_info = bfs(Node(state, 0, parent_state));
-            auto stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            input >> state_15[i];
+            //std::cout << state_15[i] << " ";
         }
+        //std::cout << std::endl;
 
-        // GBFS
-        else if(!strcmp(argv[1], "-gbfs"))
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            return_info = gbfs(Node(state, 0, parent_state));
-            auto stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        }
-
-        // A*
-        else if(!strcmp(argv[1], "-astar"))
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            return_info = a_star(Node(state, 0, parent_state));
-            auto stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        }
-
-        // IDA*
-        else if(!strcmp(argv[1], "-idastar"))
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            return_info = ida_star(Node(state, 0, parent_state));
-            auto stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        }
-
-        // IDFS
-        else if(!strcmp(argv[1], "-idfs"))
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            return_info = idfs(Node(state, 0, parent_state));
-            auto stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        }
-
+        auto start = std::chrono::high_resolution_clock::now();
+        return_info = a_star_15(Node_15(Node_15::get_long_long_from_array(state_15), 0, Node_15::get_long_long_from_array(parent_state_15))); 
+        auto stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        
+        // Show results
         std::cout << return_info.number_of_expanded_nodes << " ";
         std::cout << return_info.cost << " ";
         std::cout << duration.count() << " ";
